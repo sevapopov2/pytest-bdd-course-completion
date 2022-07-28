b07612d9216f0e9998027cfd055714dbc269a7e3
Vsevolod Popov <sevapopov13@gmail.com>
2022-07-28 22:07:15 +0300

    Change add cucumbers scenario to scenario outline

--- tests/features/cucumbers.feature
+++ tests/features/cucumbers.feature
@@ -1,9 +1,15 @@
 Feature: Cucumber Basket
 
-  Scenario: Add cucumbers to a basket
-    Given the basket has "2" cucumbers
-    When "4" cucumbers are added to the basket
-    Then the basket contains "6" cucumbers
+  Scenario Outline: Add cucumbers to a basket
+    Given the basket has "<initial>" cucumbers
+    When "<some>" cucumbers are added to the basket
+    Then the basket contains "<total>" cucumbers
+
+    Examples: Amounts
+      | initial | some | total |
+      |       0 |    2 |     2 |
+      |       5 |    2 |     7 |
+      |       3 |    3 |     6 |
 
   Scenario: Remove cucumbers from a basket
     Given the basket has "8" cucumbers
--- tests/step_defs/test_cucumbers_steps.py
+++ tests/step_defs/test_cucumbers_steps.py
@@ -1,8 +1,16 @@
 from pytest_bdd import scenarios, parsers, given, when, then
 from cucumbers import CucumberBasket
 
+# Example converters dictionary
+CONVERTERS = {
+    'initial': int,
+    'some': int,
+    'total': int
+}
 
-scenarios('../features/cucumbers.feature')
+
+scenarios('../features/cucumbers.feature', )
+
 
 # Dictionary for converting value types
 EXTRA_TYPES = {
@@ -10,12 +18,17 @@ EXTRA_TYPES = {
 }
 
 
+scenarios('../features/cucumbers.feature', example_converters=CONVERTERS)
+
+
 @given(parsers.cfparse('the basket has "{initial:Number}" cucumbers', extra_types=EXTRA_TYPES), target_fixture='basket')
+@given('the basket has <initial> cucumbers')
 def basket(initial):
     return CucumberBasket(initial_count=initial)
 
 
 @when(parsers.cfparse('"{some:Number}" cucumbers are added to the basket', extra_types=EXTRA_TYPES))
+@when('<some> cucumbers are added to the basket')
 def add_cucumbers(basket, some):
     basket.add(some)
 
@@ -26,5 +39,6 @@ def remove_cucumbers(basket, some):
 
 
 @then(parsers.cfparse('the basket contains "{total:Number}" cucumbers', extra_types=EXTRA_TYPES))
+@then('the basket contains <total> cucumbers')
 def basket_has_total(basket, total):
     assert basket.count == total
